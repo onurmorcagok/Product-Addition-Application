8677a46c07c2ce70f16c6d0a6bd0d199e71d35c9
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2020-09-09 18:40:21 +0300

    Project updated

--- product-app/src/components/NewProduct.vue
+++ product-app/src/components/NewProduct.vue
@@ -5,52 +5,69 @@
         <img
           height="128"
           class="img-responsive text-center mb-3"
-          :src="product.selectedImage == null ? '/src/assets/default.png' : product.selectedImage"/>
+          :src="product.selectedImage == null ? '/src/assets/default.png' : product.selectedImage"
+        />
         <input
           ref="file"
           type="file"
           style="display: none;"
           @change="onChange($event)"
-          class="form-control"/>
+          class="form-control"
+        />
         <button
           class="btn btn-outline-secondary"
           type="button"
-          @click="$refs.file.click()">Resim Seç</button>
+          @click="$refs.file.click()"
+        >Resim Seç</button>
       </div>
     </div>
     <div class="col-md-5">
       <div class="col-md-11 card">
         <div class="card-body">
-          <div class="form-group">
-            <label>Ürün Adı</label>
-            <input
-              type="text"
-              v-model="product.productName"
-              class="form-control"
-              placeholder="Adını Giriniz"
-            />
-          </div>
-          <div class="row">
-            <div class="form-group col-md-6">
-              <label>Ürün Adeti</label>
-              <input
-                type="text"
-                v-model="product.productCount"
-                class="form-control"
-                placeholder="Adetini Giriniz"
-              />
-            </div>
-            <div class="form-group col-md-6">
-              <label>Ürün Fiyatı</label>
-              <input
-                type="text"
-                v-model="product.productPrice"
-                class="form-control"
-                placeholder="Fiyatını Giriniz"
-              />
-            </div>
-          </div>
-          <button @click="addProduct" class="btn btn-outline-info btn-block">Ürün Ekle</button>
+          <ValidationObserver v-slot="{ handleSubmit }">
+            <form @submit.prevent="handleSubmit(addProduct)">
+              <div class="form-group">
+                <label>Ürün Adı</label>
+                <ValidationProvider name="Ürün Adı" rules="required" v-slot="{ errors }">
+                  <input
+                    type="text"
+                    v-model="product.productName"
+                    class="form-control"
+                    placeholder="Adını Giriniz"
+                  />
+                  <span class="color">{{ errors[0] }}</span>
+                </ValidationProvider>
+              </div>
+
+              <div class="row">
+                <div class="form-group col-md-6">
+                  <label>Ürün Adeti</label>
+                  <ValidationProvider name="Ürün Adeti" rules="required" v-slot="{ errors }">
+                    <input
+                      type="text"
+                      v-model="product.productCount"
+                      class="form-control"
+                      placeholder="Adetini Giriniz"
+                    />
+                    <span class="color">{{ errors[0] }}</span>
+                  </ValidationProvider>
+                </div>
+                <div class="form-group col-md-6">
+                  <label>Ürün Fiyatı</label>
+                  <ValidationProvider name="Ürün Fiyatı" rules="required" v-slot="{ errors }">
+                    <input
+                      type="text"
+                      v-model="product.productPrice"
+                      class="form-control"
+                      placeholder="Fiyatını Giriniz"
+                    />
+                    <span class="color">{{ errors[0] }}</span>
+                  </ValidationProvider>
+                </div>
+              </div>
+              <button @click="addProduct" class="btn btn-outline-info btn-block">Ürün Ekle</button>
+            </form>
+          </ValidationObserver>
         </div>
       </div>
     </div>
@@ -59,12 +76,8 @@
 
 <script>
 import { eventBus } from "../main";
-import { ValidationProvider, ValidationObserver } from 'vee-validate';
+
 export default {
-  components:{
-    ValidationProvider,
-    ValidationObserver
-  },
   data() {
     return {
       product: {
@@ -82,19 +95,26 @@ export default {
       this.product.selectedImage = URL.createObjectURL(file);
     },
     addProduct() {
-      this.product.totalPrice = this.product.productCount * this.product.productPrice;
-      eventBus.$emit("addedProduct", this.product);
-      this.product = {
-        productName: null,
-        productCount: null,
-        productPrice: null,
-        totalPrice: null,
-        selectedImage: null,
-      };
+      if (this.product.productName == null || this.product.productCount == null || this.product.productPrice == null) {
+        alert("Lütfen tüm alanları eksiksiz doldurunuz.");
+      } else {
+        this.product.totalPrice = this.product.productCount * this.product.productPrice;
+        eventBus.$emit("addedProduct", this.product);
+        this.product = {
+          productName: null,
+          productCount: null,
+          productPrice: null,
+          totalPrice: null,
+          selectedImage: null,
+        };
+      }
     },
   },
 };
 </script>
 
 <style>
+.color {
+  color: red;
+}
 </style>
--- product-app/src/main.js
+++ product-app/src/main.js
@@ -1,9 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+
+import { ValidationProvider } from 'vee-validate/dist/vee-validate.full.esm';
+import { ValidationObserver } from 'vee-validate';
+
+Vue.component('ValidationProvider', ValidationProvider);
+Vue.component('ValidationObserver', ValidationObserver);
 
 export const eventBus = new Vue();
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+  el: "#app",
+  render: (h) => h(App),
+});
